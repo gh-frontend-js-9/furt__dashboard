2a2170f7aacdcff36b9e589fdbc4c95512ba6188
Karina1213 <karinaa121213@gmail.com>
2020-03-01 16:48:48 +0200

    add navigation icon

--- src/redux/views/common/Icon.tsx
+++ src/redux/views/common/Icon.tsx
@@ -1,21 +1,34 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 
 export const Icon = () => {
-    const icons = [
-        'fa-bars',
-        'fa-line-chart',
-        'fa-envelope',
-        'fa-users',
-    ];
-    let renderIcon = icons.map((icon: string, n: number) => {
-        return <span key={n} className="sidebar__item">
-            <a href="/">
-                <i className={`${icon} fa fa--color fa--hovered fa-2x`} aria-hidden="true"> </i>
-            </a>
-        </span>
+    const icons = [{
+        name: 'fa-bars',
+        href: '/projects/'
+    }, {
+        name: 'fa-line-chart',
+        href: '#'
+    }, {
+        name: 'fa-envelope',
+        href: '/threads?sort=desc'
+    }, {
+        name: 'fa-users',
+        href: '#'
+    }];
+
+    let renderIcon = icons.map((icon: any, n: number) => {
+        return <><span className="sidebar__item">
+                        <NavLink key={n}
+                           to={icon.href}>
+                            <i key={n}
+                               className={`${icon.name} fa fa--color fa--hovered fa-2x`}
+                               aria-hidden="true"> </i>
+                        </NavLink>
+                 </span></>
     });
+
     return (
-            <>{renderIcon}</>
+        <>{renderIcon}</>
     )
 };
 
